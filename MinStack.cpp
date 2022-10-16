b8bc245b587056138b285394badc71f372411540
shyshyss <107862095+shyshyss@users.noreply.github.com>
2022-10-16 18:45:48 +0800

    Add files via upload

---
+++ MinStack.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> A, B;
+    MinStack() {}
+
+    void push(int x) {
+        A.push(x);
+        if (B.empty() || B.top() >=x) {
+            B.push(x);
+        }
+    }
+
+    void pop() {
+        if (A.top() == B.top()) {
+            B.pop();
+        }
+        A.pop();
+
+    }
+
+    int top() {
+        return A.top();
+    }
+
+    int min() {
+        return B.top();
+    }
+};
