149759505796df66b52ba42fbba807c70d12b308
shyshyss <107862095+shyshyss@users.noreply.github.com>
2022-10-20 21:09:56 +0800

    Update maxSlidingWindow.cpp

--- maxSlidingWindow.cpp
+++ maxSlidingWindow.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<queue>
-
+//使用双向队列deque存储窗口中的最大元素，使deque的队首元素为每个窗口的最大元素
 using namespace std;
 class Solution {
 public:
@@ -9,6 +9,7 @@ public:
         if (nums.size() == 0 || k == 0) return res;
         deque<int>deq;
         int i;
+        //形成窗口前
         for (i = 0; i < k; i++) {
             while (!deq.empty() && deq.back() < nums[i]) {
                 deq.pop_back();
@@ -16,6 +17,7 @@ public:
             deq.push_back(nums[i]);
         }
         res.push_back(deq.front());
+        //形成窗口后
         for (i = k; i < nums.size(); i++) {
             if (deq.front() == nums[i - k ]) {
                 deq.pop_front();
